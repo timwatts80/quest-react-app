af2525b97beea63adc4f2716a18b43a7d40976bd
timwatts80 <55103053+timwatts80@users.noreply.github.com>
2023-04-25 13:27:49 -0600

    update

--- src/layout.tsx
+++ src/layout.tsx
@@ -6,51 +6,44 @@ import { TabPanel, TabPanelProps } from './components/Tabs/Tabs2';
 import Header from 'src/components/Header/Header';
 
 const HeaderContainer = styled(Grid)(({ theme }: any) => ({
-  height: '200px',
-  backgroundColor: theme.palette.colors.grey['400'],
+    height: '200px',
+    backgroundColor: theme.palette.colors.grey['400'],
 }));
 
 const SideBar = styled(Grid)(({ theme }: any) => ({
-  width: '200px',
-  backgroundColor: theme.palette.background.default,
+    width: '200px',
+    backgroundColor: theme.palette.background.default,
 }));
 
 const Content = styled(Grid)(({ theme }: any) => ({
-  backgroundColor: theme.palette.colors.grey['500'],
-  flexGrow: 1,
+    backgroundColor: theme.palette.colors.grey['500'],
+    flexGrow: 1,
 }));
 
 const MainGrid = styled(Grid)(({ theme }: any) => ({
-  height: '100vh',
-  backgroundColor: 'lightgrey',
+    height: '100vh',
+    backgroundColor: 'lightgrey',
 }));
 
 const GridContainer = styled(Grid)(({ theme }: any) => ({
-  height: 'calc(100% - 200px)',
-  bottom: '0px',
+    height: 'calc(100% - 200px)',
+    bottom: '0px',
 }));
 
 
 function MyLayout(): JSX.Element {
-  const [value, setValue] = useState(0);
+    const [value, setValue] = useState(0);
 
-  return (
-    <MainGrid container gap={0}>
-      <HeaderContainer item xl={12} xs={12}>
-        <Header />
-        <BasicTabs />
-      </HeaderContainer>
-      <GridContainer item container xs={12}>
-        <SideBar item xs={2}>
-        </SideBar>
-        <GridContainer item xs={10}>
-          <TabPanel value={value} index={0}>
-            Item One
-          </TabPanel>
-        </GridContainer>
-      </GridContainer>
-    </MainGrid>
-  );
+    return (
+        <MainGrid container gap={0}>
+            <HeaderContainer item xl={12} xs={12}>
+                <Header />
+            </HeaderContainer>
+            <GridContainer container>
+                <BasicTabs />
+            </GridContainer>
+        </MainGrid>
+    );
 }
 
 export default MyLayout;
