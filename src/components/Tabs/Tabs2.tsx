3c2bdd0f81289ced90ae2c28b86b8e9f5f659079
timwatts80 <55103053+timwatts80@users.noreply.github.com>
2023-04-25 13:30:56 -0600

    update

--- src/components/Tabs/Tabs2.tsx
+++ src/components/Tabs/Tabs2.tsx
@@ -3,65 +3,96 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { Grid } from '@mui/material';
 
 export interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
+    children?: React.ReactNode;
+    index: number;
+    value: number;
 }
 
 export function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Grid container spacing={0} sx={{ backgroundColor: '#fff', height: '100%'}}>
+                    <Grid item xs={3}>
+                        <Box sx={{ p: 3 }}>
+                            <Tabs>
+                                <Tab label="Item One" />
+                                <Tab label="Item Two" />
+                                <Tab label="Item Three" />
+                            </Tabs>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={9}>
+                        <Box sx={{ p: 3 }}>
+                            <Typography>{children}</Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+            )}
+        </div>
+    );
 }
 
 function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
 }
 
 export default function BasicTabs() {
-  const [value, setValue] = React.useState(0);
+    const [value, setValue] = React.useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
 
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-    </Box>
-  );
+    const tabStyles = {
+        '&:hover, &.Mui-selected': {
+            bgcolor: 'white',
+        },
+    };
+
+    return (
+        <Box sx={{ height: 'auto', width: '100%', backgroundColor: '#ff0000' }}>
+            <Box sx={{ position: 'relative', width: '100%', borderColor: 'divider' }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    TabIndicatorProps={{ sx: { top: 0 } }}
+                    aria-label="basic tabs example"
+                    sx={{
+                        position: 'absolute',
+                        bottom: '0px',
+                        left: '50%',
+                        transform: 'translateX(-50%)',
+                    }}
+                >
+                    <Tab label="Item One" {...a11yProps(0)} sx={tabStyles} />
+                    <Tab label="Item Two" {...a11yProps(1)} sx={tabStyles} />
+                    <Tab label="Item Three" {...a11yProps(2)} sx={tabStyles} />
+                </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+                Item One
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item Three
+            </TabPanel>
+        </Box>
+    );
 }
